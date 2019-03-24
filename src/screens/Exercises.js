e46fcc926632bf3e02bb1ad31fc6287ddad08d04
Iv4n84110 <vanka3087@gmail.com>
2019-03-24 22:24:15 +0300

    Доработана страница'Exercises'

--- src/screens/Exercises.js
+++ src/screens/Exercises.js
@@ -1,24 +1,82 @@
 import React, { Component } from "react";
-import { StatusBar } from "react-native";
+import { Button, View, Text } from 'react-native';
+import { createAppContainer, createStackNavigator } from 'react-navigation';
 import {
   Container,
   Content,
-  Text
+  List,
+  ListItem,
+  Thumbnail,
+  Left,
+  Body,
+  Right,
 } from "native-base";
 
-import Menu from "../components/Menu";
-
-class Statistics extends Component {
+class ExercisesScreen extends Component {
   render() {
     return (
       <Container>
-        <StatusBar backgroundColor="rgba(0, 0, 0, 0.3)" barStyle="light-content" translucent/>
         <Content>
-          <Text>Exercieses</Text>
+          <List>
+              <ListItem thumbnail>
+                <Left>
+                  <Thumbnail square source={{ uri: 'Image URL' }} />
+                </Left>
+                <Body>
+                  <Text>Sankhadeep</Text>
+                  <Text note numberOfLines={1}>Its time to build a difference . .</Text>
+                </Body>
+                <Right>
+                  <Button 
+                  title="Try"
+                  transparent 
+                  onPress={() => this.props.navigation.navigate('Exercise')} 
+                  />
+                </Right>
+              </ListItem>
+          </List>
         </Content>
       </Container>
     );
   }
 }
 
-export default Statistics;
+class ExerciseScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Exercise Screen</Text>
+        <Button
+          title="Go back"
+          onPress={() => this.props.navigation.goBack()}
+        />
+      </View>
+    );
+  }
+}
+
+const RootStack = createStackNavigator(
+  {
+    Exercises: {
+      screen: ExercisesScreen,
+    },
+    Exercise: {
+      screen: ExerciseScreen,
+      footerMode: "none",
+    },
+  },
+  {
+    initialRouteName: 'Exercises',
+    headerMode: "none",
+  }
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <AppContainer/>
+    );
+  }
+}
